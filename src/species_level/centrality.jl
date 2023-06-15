5b6cbbd6f34a316b29749c0ab3ee14da05cda6a7
Timothée Poisot <timothee.poisot@umontreal.ca>
2023-06-15 09:25:23 -0400

    doc: centrality

--- src/species_level/centrality.jl
+++ src/species_level/centrality.jl
@@ -12,6 +12,11 @@ abstract type CentralityMethod end
 """
     ClosenessCentrality
 
+Closeness centrality is defined as the reciprocal of farness, *i.e.* 1 over the
+sum of shortest paths from the source node to all other nodes. In the case of
+probabilistic networks, we use the random walk approximation of closeness
+centrality, where the distance is defined as first passage time.
+
 ###### References
 
 [Bavelas1950Communication](@cite)
