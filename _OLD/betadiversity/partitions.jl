4cf7f19f89a8e225e32617ec76227664d6fcab85
Timothée Poisot <timothee.poisot@umontreal.ca>
2023-06-16 09:40:20 -0400

    feat: beta WN

--- _OLD/betadiversity/partitions.jl
+++ _OLD/betadiversity/partitions.jl
@@ -74,30 +74,3 @@ function βos(X::T, Y::T) where {T<:UnipartiteNetwork}
     c = links(Yi)-a
     return (a=a, b=b, c=c)
 end
-
-
-"""
-    βwn(X::T, Y::T) where {T<:BinaryNetwork}
-
-Whole network
-
-#### References
-
-Canard, E.F., Mouquet, N., Mouillot, D., Stanko, M., Miklisova, D., Gravel, D.,
-2014. Empirical evaluation of neutral interactions in host-parasite networks.
-The American Naturalist 183, 468–479. https://doi.org/10.1086/675363
-
-Poisot, T., Canard, E., Mouillot, D., Mouquet, N., Gravel, D., 2012. The
-dissimilarity of species interaction networks. Ecol. Lett. 15, 1353–1361.
-https://doi.org/10.1111/ele.12002
-
-Poisot, T., Cirtwill, A.R., Cazelles, K., Gravel, D., Fortin, M.-J., Stouffer,
-D.B., 2016. The structure of probabilistic networks. Methods in Ecology and
-Evolution 7, 303–312. https://doi.org/10.1111/2041-210X.12468
-"""
-function βwn(X::T, Y::T) where {T<:BinaryNetwork}
-    a = links(intersect(X,Y))
-    c = links(X)-a
-    b = links(Y)-a
-    return (a=a, b=b, c=c)
-end
--- src/meta_level/partitions.jl
+++ src/meta_level/partitions.jl
@@ -3,6 +3,9 @@
 
 The [`betadiversity`](@ref) methods all use a subtype of `BetaDivComponent` as
 their first argument, to determine which component should be measured.
+
+All of the partitions follow the [Koleff2003Measuring](@cite) approach, where
+the beta diversity is measured on the cardinality of sets.
 """
 abstract type BetaDivComponent end
 
@@ -15,15 +18,12 @@ abstract type βS <: BetaDivComponent end
 
 """
     βOS
-
-Specie
 """
 abstract type βOS <: BetaDivComponent end
 
 """
     βWN
 
-Specie
 """
 abstract type βWN <: BetaDivComponent end
 
@@ -33,6 +33,10 @@ abstract type βWN <: BetaDivComponent end
 Species-level β diversity between networks. By default, this return the species
 dissimilarity for the entire network. An optional last argument `dims` can be
 used, to specify top (`1`) and bottom (`2`) levels.
+
+###### References
+
+[Koleff2003Measuring](@cite)
 """
 function betadiversity(
     ::Type{βS},
@@ -56,5 +60,50 @@ end
     part = betadiversity(βS, U, V)
     @test part.shared == 1
     @test part.right == 1
+    @test part.left == 2
+end
+
+"""
+    betadiversity(::Type{βWN},U::T,V::T) where {T <: SpeciesInteractionNetwork{<:Partiteness, <:Binary}}
+
+Network-level β diversity between networks. By default, this return the species
+dissimilarity for the entire network. An optional last argument `dims` can be
+used, to specify top (`1`) and bottom (`2`) levels.
+
+###### References
+
+[Poisot2012dissimilarity](@cite)
+
+[Canard2014Empirical](@cite)
+
+[Poisot2022Dissimilarity](@cite)
+"""
+function betadiversity(
+    ::Type{βWN},
+    U::T,
+    V::T
+) where {T <: SpeciesInteractionNetwork{<:Partiteness, <:Binary}}
+    shared = length(intersect(U, V))
+    right = length(V) - shared
+    left = length(U) - shared
+    return (; left, shared, right)
+end
+
+@testitem "We can get the βWN for a unipartite network" begin
+    Nu = Unipartite([:A, :B, :C])
+    Eu = Binary(zeros(Bool, (richness(Nu,1), richness(Nu,2))))
+    Nv = Unipartite([:A, :C, :D])
+    Ev = Binary(zeros(Bool, (richness(Nv,1), richness(Nv,2))))
+    U = SpeciesInteractionNetwork(Nu, Eu)
+    V = SpeciesInteractionNetwork(Nv, Ev)
+    U[:A, :C] = true
+    U[:A, :B] = true
+    U[:B, :C] = true
+    V[:A, :C] = true
+    V[:C, :D] = true
+    V[:A, :D] = true
+    part = betadiversity(βWN, U, V)
+    @test part.shared == 1
+    @test part.right == 2
     @test part.left == 2
 end 
