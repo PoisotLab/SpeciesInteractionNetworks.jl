393e16ee332f89f7848ae6ca0ddd5dbfb42247ed
Timothée Poisot <timothee.poisot@umontreal.ca>
2023-06-16 14:51:10 -0400

    doc: bib notice

--- docs/src/meta_level/betadiversity.md
+++ docs/src/meta_level/betadiversity.md
@@ -13,6 +13,10 @@ BetaDivComponent
 βWN
 ```
 
+!!! tip "What about the species turnover component?"
+
+    In [Poisot2012dissimilarity](@cite), we introduced the idea that the impact of species turnover can often be expressed as the difference between the whole-network ([`βWN`](@ref)) and overlapping-species ([`βOS`](@ref)) dissimilarities. This is only true for *some* measures. Following the arguments laid out in [Poisot2022Dissimilarity](@cite), we have *not* added this as a built-in function. If there is a need to measure the impact of turnover, it is recommended to express it as (wn-os)/wn. 
+
 ## Beta diversity measures
 
 The [`betadiversity`](@ref) function will *always* return a named tuple with three entries, named
--- src/meta_level/partitions.jl
+++ src/meta_level/partitions.jl
@@ -5,7 +5,15 @@ The [`betadiversity`](@ref) methods all use a subtype of `BetaDivComponent` as
 their first argument, to determine which component should be measured.
 
 All of the partitions follow the [Koleff2003Measuring](@cite) approach, where
-the beta diversity is measured on the cardinality of sets.
+the beta diversity is measured on the cardinality of sets. Specifically, all
+[`betadiversity`](@ref) functions will return a named tuple with three fields,
+called `shared`, `left`, and `right`. These represent, respectively, the number
+of items common to both networks, the number of items unique to the first
+argument, and the number of items unique to the second argument.
+
+###### References
+
+[Koleff2003Measuring](@cite)
 """
 abstract type BetaDivComponent end
 
