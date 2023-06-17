3d9015c16c9b0afdeddc53467a7b40a84d5a0f4f
Timothée Poisot <timothee.poisot@umontreal.ca>
2023-06-17 12:24:17 -0400

    doc: refs

--- src/random/linearfilter.jl
+++ src/random/linearfilter.jl
@@ -23,17 +23,103 @@ function linearfilter(N::SpeciesInteractionNetwork{<:Partiteness, <:Binary}; α:
 
 end
 
+@testitem "The linearfilter function returns the correct type" begin
+    nodes = Unipartite([:A, :B, :C, :D, :E, :F])
+    edges = Binary(rand(Bool, (richness(nodes,1), richness(nodes, 2))))
+    N = SpeciesInteractionNetwork(nodes, edges)
+    R = linearfilter(N)
+    @test typeof(R.nodes) <: Unipartite
+    @test typeof(R.edges) <: Probabilistic
+    @test richness(N) == richness(R)
+end
+
+@testitem "The linearfilter with α[4] = 1 is the average only" begin
+    import Statistics
+    nodes = Unipartite([:A, :B, :C, :D, :E, :F])
+    edges = Binary(rand(Bool, (richness(nodes,1), richness(nodes, 2))))
+    N = SpeciesInteractionNetwork(nodes, edges)
+    R = linearfilter(N; α=[0.0, 0.0, 0.0, 1.0])
+    for interaction in N
+        @test R[interaction[1], interaction[2]] == Statistics.mean(N.edges.edges)
+    end
+end
+
+@testitem "The linearfilter with α[1] = 1 is itself" begin
+    import Statistics
+    nodes = Unipartite([:A, :B, :C, :D, :E, :F])
+    edges = Binary(rand(Bool, (richness(nodes,1), richness(nodes, 2))))
+    N = SpeciesInteractionNetwork(nodes, edges)
+    R = linearfilter(N; α=[1.0, 0.0, 0.0, 0.0])
+    for interaction in N
+        @test R[interaction[1], interaction[2]] == 1.0
+    end
+end
+
 """
     nullmodel(::Type{Degree}, N::SpeciesInteractionNetwork{<:Partiteness, <:Binary})
 
-TODO
+Returns a probabilistic network under the null model that all interactions
+occurr with a probability equal to the average of their generality and
+vulnerability, *i.e.* proportionally to how many links the species involved are
+establishing:
+    
+``P(i \\rightarrow j) = \\frac{1}{2}\\left(\\frac{\\sum A_{i \\rightarrow \\cdot}}{|B|} + \\frac{\\sum A_{\\cdot \\rightarrow j}}{|T|} \\right)``
 
 ###### References
 
-[Bascompte2003nested](@citep)
-
+[Bascompte2003nested](@citet)
 """
 nullmodel(::Type{Degree}, N::SpeciesInteractionNetwork{<:Partiteness, <:Binary}) = linearfilter(N; α=[0.0, 1.0, 1.0, 0.0])
+
+"""
+    nullmodel(::Type{Connectance}, N::SpeciesInteractionNetwork{<:Partiteness, <:Binary})
+
+Returns a probabilistic network under the null model that all interactions
+occurr with a probability equal to the network connectance, *i.e.* in a network
+with ``L`` links, and ``|T|`` species on the top and ``|B|`` species on the
+bottom,
+
+``P(i \\rightarrow j) = \\frac{L}{|T|\\times |B|}``
+
+Note that for [`Unipartite`](@ref) networks, ``|T| = |B| = |S|`` (the two sides
+have the same number of species).
+
+###### References
+
+[Fortuna2006Habitat](@citet)
+"""
 nullmodel(::Type{Connectance}, N::SpeciesInteractionNetwork{<:Partiteness, <:Binary}) = linearfilter(N; α=[0.0, 0.0, 0.0, 1.0])
+
+"""
+    nullmodel(::Type{Generality}, N::SpeciesInteractionNetwork{<:Partiteness, <:Binary})
+
+Returns a probabilistic network under the null model that interactions happen
+proportionally to the generality of the species, *i.e.* their expected number of
+outgoing links:
+
+``P(i \\rightarrow j) = \\frac{\\sum A_{i \\rightarrow \\cdot}}{|B|}``
+
+###### References
+
+[Poisot2013Facultative](@citet)
+
+[Weitz2013Phagebacteria](@citet)
+"""
 nullmodel(::Type{Generality}, N::SpeciesInteractionNetwork{<:Partiteness, <:Binary}) = linearfilter(N; α=[0.0, 1.0, 0.0, 0.0])
+
+"""
+    nullmodel(::Type{Generality}, N::SpeciesInteractionNetwork{<:Partiteness, <:Binary})
+
+Returns a probabilistic network under the null model that interactions happen
+proportionally to the vulnerability of the species, *i.e.* their expected number of
+incomin links:
+
+``P(i \\rightarrow j) = \\frac{\\sum A_{\\cdot \\rightarrow j}}{|T|}``
+
+###### References
+
+[Poisot2013Facultative](@citet)
+
+[Weitz2013Phagebacteria](@citet)
+"""
 nullmodel(::Type{Vulnerability}, N::SpeciesInteractionNetwork{<:Partiteness, <:Binary}) = linearfilter(N; α=[0.0, 0.0, 1.0, 0.0])
