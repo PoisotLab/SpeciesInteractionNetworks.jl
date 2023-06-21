ebc2c23f7876279d2d743cdede5fadd9a1724e58
Timothée Poisot <timothee.poisot@umontreal.ca>
2023-06-21 08:52:27 -0400

    perf: interactions is now fast as fuck

--- src/basics/interactions.jl
+++ src/basics/interactions.jl
@@ -7,13 +7,18 @@ interaction, the species receiving the interaction, and the value of the
 interaction.
 """
 function interactions(N::SpeciesInteractionNetwork)
-    IN = Array{eltype(N)}(undef, length(N))
-    i = 1
-    for int in N
-        IN[i] = int
-        i += 1
+    edgelist = Array{eltype(N)}(undef, length(N))
+    cursor = 1
+    for i in axes(N, 1)
+        for j in axes(N, 2)
+            if !iszero(N[i,j])
+                edgelist[cursor] = (species(N,1)[i],species(N,2)[j],N[i,j]) 
+                cursor += 1
+            end
+        end
     end
-    return IN
+    @assert (cursor-1) == length(N) == length(edgelist)
+    return edgelist
 end
 
 @testitem "We can get the interactions in a network" begin
