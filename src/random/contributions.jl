15825afd457af5560e63378f8336bddfa9a08f0d
Timothée Poisot <timothee.poisot@umontreal.ca>
2023-06-21 09:13:15 -0400

    perf: contributions

--- src/random/contributions.jl
+++ src/random/contributions.jl
@@ -33,11 +33,36 @@ function nullmodel(::Type{C}, N::SpeciesInteractionNetwork{<:Partiteness{T}, <:B
 end
 
 """
-TODO
+    speciescontribution(::Type{C}, N::SpeciesInteractionNetwork{<:Partiteness{T}, <:Binary}, sp::T, f, args...; replicates=999, kwargs...) where {C <: PermutationConstraint, T}
+
+Returns the contribution of species `sp` to the value of `f(N, args...;
+kwargs...)`, after [Saavedra2011Strong](@citet). Specifically, this function
+will generate a species-specific [`nullmodel`](@ref), and then update *in-place*
+a copy of the network where interactions of species `sp` are randomly re-drawn
+according to the probabilities under the null model.
+
+Note that for the sake of generality, the function `f` can accept both
+positional and keyword arguments.
+
+The contribution of a species to the score is defined as
+
+``\\frac{f(N)-\\mu_f}{\\sigma_f}``
+
+where μ and σ are respectively the average and standard deviation of the values
+of the measure `f` on the randomized networks.
+
+In order to ensure that the networks generated under the null model are
+informative, we constrain the number of interactions of `sp` in the randomized
+network to be equal to the number of interactions of `sp` in the original
+network.
+
+###### References
+
+[Saavedra2011Strong](@citet*)
 """
 function speciescontribution(::Type{C}, N::SpeciesInteractionNetwork{<:Partiteness{T}, <:Binary}, sp::T, f, args...; replicates=999, kwargs...) where {C <: PermutationConstraint, T}
     R = nullmodel(C, N, sp)
-    M = copy(N) # We will update this one in place
+    M = copy(N)
     v0 = f(N, args...; kwargs...)
     vx = zeros(typeof(v0), replicates)
     for replicate in 1:replicates
