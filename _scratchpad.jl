881422cc1ff95a6747cacfed1350a5df9b1bdc2a
Timothée Poisot <timothee.poisot@umontreal.ca>
2023-06-23 22:34:06 -0400

    misc: tests

--- _scratchpad.jl
+++ _scratchpad.jl
@@ -1,17 +1,27 @@
 using Revise
 using SpeciesInteractionNetworks
 import DelimitedFiles
+import Statistics
 
 data_path = joinpath(@__DIR__, "data", "nz_stream")
 
 netfiles = filter(endswith(".csv"), readdir(data_path; join=true))
 
-X = DelimitedFiles.readdlm(netfiles[1], ',')
-A = permutedims(X[2:end,2:end])
-S = [replace(s, r"^(.+) \(N=\d+\)$" => s"\1") for s in X[1,2:end]]
-nodes = Unipartite(S)
-edges = Binary(Bool.(A))
-N = SpeciesInteractionNetwork(nodes, edges)
+for nf in netfiles
+    X = DelimitedFiles.readdlm(nf, ',')
+    A = permutedims(X[2:end,2:end])
+    S = [replace(s, r"^(.+) \(N=\d+\)$" => s"\1") for s in X[1,2:end]]
+    nodes = Unipartite(S)
+    edges = Binary(Bool.(A))
+    N = SpeciesInteractionNetwork(nodes, edges)
+    @info last(splitpath(nf))
+    R = [structuralmodel(NicheModel, richness(N), connectance(N)) for i in 1:150]
+    r = complexity.(R)
+    m, v = Statistics.mean(r), Statistics.std(r)
+    x = complexity(N)
+    @info " -> $((x - m)/v)"
+end
+
 predecessors(N, "Unidentified detritus")
 
 M = motifs(Unipartite, 3)[1]
