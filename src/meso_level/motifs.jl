d19c28af79a5a444c27bd5f92b7646e2c49c37ad
Timothée Poisot <timothee.poisot@umontreal.ca>
2023-06-23 20:41:59 -0400

    feat: findmotif (fast) for unipartite networks

--- src/meso_level/motifs.jl
+++ src/meso_level/motifs.jl
@@ -1,11 +1,23 @@
-function findmotif(N::SpeciesInteractionNetwork{<:Unipartite, <:Binary}, M::SpeciesInteractionNetwork{<:Unipartite, <:Binary})
+"""
+    findmotif(M::SpeciesInteractionNetwork{<:Unipartite, <:Binary}, N::SpeciesInteractionNetwork{<:Unipartite, <:Binary})
+
+Search for all groups of species in `N` that match the motif `M` -- note that
+the motif to search is the *first* argument.
+"""
+function findmotif(M::SpeciesInteractionNetwork{<:Unipartite, <:Binary}, N::SpeciesInteractionNetwork{<:Unipartite, <:Binary})
     motif_config = _permutations(M)
-    sp_combinations = combinations(species(N), richness(M))
+    sp_combinations = combinations(1:richness(N), richness(M))
     hits = zeros(Bool, length(sp_combinations))
     cursor = 1
     for spcomb in sp_combinations
-        hits[cursor] = Array(subgraph(N, spcomb)) in motif_config
+        hits[cursor] = N[spcomb,spcomb] in motif_config
         cursor += 1
     end
-    return collect(sp_combinations)[hits]
+    nads = collect(sp_combinations)[hits]
+    T = eltype(N.nodes)
+    sps = Vector{Tuple{T,T,T}}(undef, length(nads))
+    for (i,nad) in enumerate(nads)
+        sps[i] = tuple(species(N)[nad]...)
+    end
+    return sps
 end
